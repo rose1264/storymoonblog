1eafbd5c88e049b46e2b8bcd9a685753e4845670
Rose <rose1264@gmail.com>
2017-12-09 11:08:41 -0500

    change Welcome to Pages as a blog

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'welcome/home', to:'welcome#home'
-  get 'welcome/about', to:'welcome#about'
+  root 'pages#home'
+  get 'about', to:'pages#about'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
